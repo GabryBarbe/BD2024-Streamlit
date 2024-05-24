01ea18425ba454672a7d13b623e6b1969bd0a6bb
Gabriele Barbero <barbero.gabriele03@gmail.com>
2024-05-25 00:26:01 +0200

    tab_staff + 1/2 tab_clienti - 24052024

--- pages/1_📈_Analisi.py
+++ pages/1_📈_Analisi.py
@@ -30,6 +30,36 @@ def create_tab_prodotti(tab):
             df_prodotti = pd.DataFrame(prodotti)
             st.dataframe(df_prodotti, use_container_width=True)
 
+def create_tab_staff(tab):
+    col1, col2 = tab.columns(2)
+
+    query_president = "SELECT firstName, lastName FROM employees WHERE jobTitle='President';"
+    president = execute_query(st.session_state["connection"], query_president).mappings().first()
+    query_vpsales = "SELECT firstName, lastName FROM employees WHERE jobTitle='VP Sales';"
+    vpsales = execute_query(st.session_state["connection"], query_vpsales).mappings().first()
+
+    col1.markdown(f"### **:blue[PRESIDENT: ] {president['firstName']} {president['lastName']}**")
+    col2.markdown(f"### **:orange[VP SALES: ] {vpsales['firstName']} {vpsales['lastName']}**")
+
+    query_staff = "SELECT jobTitle, COUNT(*) AS numDipendenti FROM employees GROUP BY jobTitle;"
+    staff = execute_query(st.session_state["connection"], query_staff)
+    df = pd.DataFrame(staff)
+
+    tab.markdown("### Componenti Staff")
+    tab.bar_chart(df, x='jobTitle', y='numDipendenti')
+
+def create_tab_clienti(tab):
+    col1, col2 = tab.columns(2)
+
+    with col1:
+        st.markdown("### Distribuzione clienti nel mondo")
+        query="SELECT COUNT(*) AS numeroClienti, country FROM customers GROUP BY country ORDER BY numeroClienti DESC ;"
+        result = execute_query(st.session_state["connection"], query)
+        df = pd.DataFrame(result)
+        st.dataframe(df, use_container_width=True)
+
+    with col2:
+        st.markdown("### Clienti con maggior _credit line_ negli USA")
 
 if __name__ == "__main__":
     st.title("📈 Analisi")
@@ -39,4 +69,6 @@ if __name__ == "__main__":
 
     if check_connection():
         create_tab_prodotti(tab=tab_prodotti)
+        create_tab_staff(tab=tab_staff)
+        create_tab_clienti(tab=tab_clienti)
     
